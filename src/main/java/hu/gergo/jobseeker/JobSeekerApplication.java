445da86da454b8acd91466f9ad4bafe1aa35e21c
Gergő Török <tgergo1997@gmail.com>
2023-06-07 16:38:24 +0200

    Init

---
+++ src/main/java/hu/gergo/jobseeker/JobSeekerApplication.java
@@ -0,0 +1,12 @@
+package hu.gergo.jobseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobSeekerApplication.class, args);
+    }
+
+}
