76a16b6f54728f1ab2ba1e855f3b42ebcb8eed9d
Gergő Török <tgergo1997@gmail.com>
2023-06-08 19:17:21 +0200

    Client registration created

---
+++ src/main/java/hu/gergo/jobseeker/model/Client.java
@@ -0,0 +1,59 @@
+package hu.gergo.jobseeker.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.UUID;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "A név kötelező")
+    @Size(max = 100, message = "A név legfeljebb 100 karakter lehet")
+    private String name;
+
+    @NotBlank(message = "Az e-mail cím kötelező")
+    @Email(message = "Érvénytelen e-mail cím formátum")
+    @Column(unique = true)
+    private String email;
+
+    private final String apiKey;
+
+    public Client() {
+        apiKey = UUID.randomUUID().toString();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+}
---
+++ src/main/java/hu/gergo/jobseeker/service/ClientService.java
@@ -0,0 +1,28 @@
+package hu.gergo.jobseeker.service;
+
+import hu.gergo.jobseeker.exception.EmailAlreadyExists;
+import hu.gergo.jobseeker.model.Client;
+import hu.gergo.jobseeker.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+    private final ClientRepository repository;
+
+    @Autowired
+    public ClientService(ClientRepository repository) {
+        this.repository = repository;
+    }
+
+    public String registerClient(Client client) {
+        if (repository.existsByEmail(client.getEmail())) {
+            throw new EmailAlreadyExists("Az email már foglalt.");
+        }
+
+        repository.save(client);
+
+        return client.getApiKey();
+    }
+}
