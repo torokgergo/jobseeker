45e781b47aedf2f2b917676fba2949315a2797b7
Gergő Török <tgergo1997@gmail.com>
2023-06-09 13:07:43 +0200

    Ids removed from import.sql, existsByApiKey param changed to uuid from string

--- src/main/java/hu/gergo/jobseeker/model/Client.java
+++ src/main/java/hu/gergo/jobseeker/model/Client.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 public class Client {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "A név kötelező")
--- src/main/java/hu/gergo/jobseeker/model/Position.java
+++ src/main/java/hu/gergo/jobseeker/model/Position.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.Size;
 public class Position {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "A megnevezés kötelező")
